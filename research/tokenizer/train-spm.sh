e4030b8a1050dae33d674979f094781ed171cc1f
Mykola Haltiuk <kaljandlux@gmail.com>
2023-08-16 00:36:16 +0200

    updated scriptts to be working

--- research/tokenizer/train-spm.sh
+++ research/tokenizer/train-spm.sh
@@ -1,12 +1,16 @@
-python train-sentencepiece.py \
-  --input=$DATA_DIR/train.txt \
-  --model_prefix=$DATA_DIR/spm \
-  --self_test_sample_size=??? \
-  --character_coverage=??? \
-  --input_sentence_size=??? \
-  --num_threads=??? \
-  --num_subprocess=??? \
-  --user_defined_symbols_file=$DATA_DIR/user_defined_symbols.txt \
-  --byte_fallback=??? \
-  --normalization_rule_tsv=$DATA_DIR/normalization_rule.tsv \
-  --add_dummy_prefix=???
+/usr/bin/time -v python3 train-sentencepiece.py \
+  --input="$DATA_DIR"/train.txt \
+  --model-prefix="$DATA_DIR"/spm \
+  --self-test-sample-size=1000 \
+  --character-coverage=0.9995 \
+  --input-sentence-size=10000000 \
+  --num-threads=12 \
+  --byte-fallback=true |& tee "$DATA_DIR"/spm_train.log
+#  --normalization-rule-tsv=$DATA_DIR/normalization_rule.tsv
+#  --user-defined-symbols-file=$DATA_DIR/user_defined_symbols.txt \
+
+
+# --self_test_sample_size=1000  -  what does it do at all???
+# --character_coverage=0.9995  -  may change after metrics analysis
+# --input_sentence_size=5000000  -  FIXME change to bigger value after tests
+# --byte_fallback=true  -  I need to verify if this does not introduce any problem
