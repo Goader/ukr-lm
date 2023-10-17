3c2d6e59e2095f7a189a3124c34e32fe80b23048
Mykola Haltiuk <kaljandlux@gmail.com>
2023-10-17 16:45:04 +0200

    ukrainian news classification script fixed + inference script

---
+++ benchmarks/ukrainian-news-classification/inference.py
@@ -0,0 +1,80 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import tqdm
+import csv
+
+import torch
+import numpy as np
+from transformers import TrainingArguments, Trainer, AutoModelForSequenceClassification, DataCollatorWithPadding
+from transformers.models.bert.modeling_bert import BertForSequenceClassification, BertForMaskedLM
+from datasets import load_dataset
+from sklearn.metrics import f1_score, accuracy_score
+
+from ukrlm.tokenizers import LibertaTokenizer
+from ukrlm.utils import load_ckpt
+
+
+DEFAULT_TOKENIZER_PATH = \
+    Path(__file__).parent.parent.parent / 'research' / 'tokenizer' / 'experiment-5-overall-v2' / 'spm.model'
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--checkpoint', type=str, required=True, help='path to the model dir')
+    parser.add_argument('--tokenizer', type=str, default=DEFAULT_TOKENIZER_PATH, help='path to the tokenizer')
+    parser.add_argument('--data', type=str, required=True, help='path to the inference data (CSV file)')
+    parser.add_argument('--output', type=str, required=True, help='path to the output CSV file')
+    parser.add_argument('--batch_size', type=int, default=1, help='batch size')
+    args = parser.parse_args()
+
+    model = AutoModelForSequenceClassification.from_pretrained(args.checkpoint)
+    tokenizer = LibertaTokenizer(args.tokenizer)
+
+    dataset = load_dataset(
+        'csv',
+        data_files=args.data,
+        split='train'
+    )
+
+    def concatenate_title_and_text(example):
+        return {
+            'id': example['Id'],
+            'text': example['title'] + '. ' + example['text'],
+        }
+
+    dataset = dataset.map(concatenate_title_and_text)
+
+    def tokenize(batch):
+        return tokenizer(
+            batch['text'],
+            padding='max_length',
+            truncation=True,
+            max_length=512,
+            return_tensors='pt'
+        )
+
+    dataset = dataset.map(tokenize, batched=True, batch_size=256)
+    dataset = dataset.remove_columns(['Id', 'title', 'text'])
+    collator = DataCollatorWithPadding(tokenizer, padding='longest', max_length=512)
+
+    model.eval()
+    with open(args.output, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=['Id','Predicted'])
+        for input in tqdm.tqdm(dataset.iter(
+                batch_size=args.batch_size,
+                drop_last_batch=False
+        ), total=len(dataset) // args.batch_size):
+
+            document_ids = input['id']
+            input = collator({
+                'input_ids': input['input_ids'],
+                'attention_mask': input['attention_mask']
+            })
+
+            with torch.no_grad():
+                logits = model(**input).logits
+
+            predicted_class_ids = logits.argmax(dim=-1)
+
+            for document_id, predicted_class_id in zip(document_ids, predicted_class_ids):
+                writer.writerow({'Id': document_id, 'Predicted': predicted_class_id})
