88ad3872d0058b7ae91553aec29c336d25072ea9
Goader <kaljandlux@gmail.com>
2024-02-02 17:03:46 +0100

    updates to finetuning

--- benchmarks/ukrainian-news-classification/train.py
+++ benchmarks/ukrainian-news-classification/train.py
@@ -84,9 +84,9 @@ if __name__ == '__main__':
             evaluation_strategy='epoch',
             save_strategy='epoch',
             learning_rate=2e-5,
-            per_device_train_batch_size=16,
-            per_device_eval_batch_size=16,
-            num_train_epochs=3,
+            per_device_train_batch_size=32,
+            per_device_eval_batch_size=32,
+            num_train_epochs=10,
             weight_decay=0.01,
             load_best_model_at_end=True,
             metric_for_best_model='macro_f1',
@@ -99,5 +99,7 @@ if __name__ == '__main__':
     )
 
     trainer.train()
+
+    print('Evaluating on test dataset')
     trainer.evaluate(dataset['test'])
     trainer.save_model('model')
